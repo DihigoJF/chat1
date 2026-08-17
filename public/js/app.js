c0c86002a2d2e5c3c1c78ca2ee2e1789e3067d70
JFranzA-dev <78769005+DihigoJF@users.noreply.github.com>
2026-08-17 18:24:38 -0500

    Delete public directory

--- public/js/app.js
+++
@@ -1,4 +0,0 @@
-// app.js - Punto de entrada
-console.log('Aplicación de chat iniciada');
-// Los módulos connection.js y chat.js ya se autoinicializan.
-// Si se necesita alguna configuración adicional, se coloca aquí.
